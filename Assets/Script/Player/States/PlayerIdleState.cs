a351adf92202e04b12d7d1132ef7a18ab750c857
DongHaaaa <dongdaiha9@gmail.com>
2026-09-26 12:19:14 +0700

    Add PlayerIdleState

---
+++ Assets/Script/Player/States/PlayerIdleState.cs
@@ -0,0 +1,27 @@
+public class PlayerIdleState : PlayerState
+{
+    public PlayerIdleState(Player player, PlayerStateMachine stateMachine)
+        : base(player, stateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    public override void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+}

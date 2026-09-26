31a45f7266f0211b41f0a5765e8b7304350d6880
DongHaaaa <dongdaiha9@gmail.com>
2026-09-26 13:06:52 +0700

    Add PlayerJumpState

---
+++ Assets/Script/Player/States/PlayerJumpState.cs
@@ -0,0 +1,39 @@
+public class PlayerJumpState : PlayerState
+{
+    public PlayerJumpState(Player player, PlayerStateMachine stateMachine)
+        : base(player, stateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        player.Animator.SetBool("IsRunning", false);
+        player.Animator.SetBool("OnGround", false);
+        player.Animator.SetBool("IsJumping", true);
+
+        player.Movement.Jump();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        float moveX = player.MoveInput.x;
+
+        player.Movement.Flip(moveX);
+        player.Animator.SetFloat("yVelocity", player.VerticalVelocity);
+
+        if (player.VerticalVelocity < 0f)
+        {
+            stateMachine.ChangeState(player.FallState);
+        }
+    }
+
+    public override void FixedUpdate()
+    {
+        base.FixedUpdate();
+        player.Movement.Move(player.MoveInput.x);
+    }
+}

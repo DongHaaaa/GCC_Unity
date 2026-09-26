dc0ccca7b850ff2780654e2988d0067728d68355
DongHaaaa <dongdaiha9@gmail.com>
2026-09-26 11:53:19 +0700

    Add PlayerState.cs

---
+++ Assets/Script/Player/StateMachine/PlayerState.cs
@@ -0,0 +1,27 @@
+public abstract class PlayerState
+{
+    protected Player player;
+    protected PlayerStateMachine stateMachine;
+
+    protected PlayerState(Player player, PlayerStateMachine stateMachine)
+    {
+        this.player = player;
+        this.stateMachine = stateMachine;
+    }
+
+    public virtual void Enter()
+    {
+    }
+
+    public virtual void Update()
+    {
+    }
+
+    public virtual void FixedUpdate()
+    {
+    }
+
+    public virtual void Exit()
+    {
+    }
+}
